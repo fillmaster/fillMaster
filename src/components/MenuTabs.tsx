25ce0767668c854dc2499c2a96b7bae406c81d43
Daniel Callus <danielcallus@gmail.com>
2022-05-29 12:20:37 +0100

    added default tab of settings

--- src/components/MenuTabs.tsx
+++ src/components/MenuTabs.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
+import { useState, SyntheticEvent } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 
 const BasicTabs = () => {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState('Settings');
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (event: SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
 
   return (
     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
       <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-        <Tab label="Settings" value="1" />
-        <Tab label="How to use" value="2" />
-        <Tab label="About" value="3" />
+        <Tab label="Settings" value="Settings" />
+        <Tab label="How to use" value="HowTo" />
+        <Tab label="About" value="About" />
       </Tabs>
     </Box>
   );
