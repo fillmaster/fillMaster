f3e7958546270ae9ba4dd54f555db2e8b7fd1427
Daniel Callus <danielcallus@gmail.com>
2022-05-31 19:10:36 +0100

    actually centered text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,20 +12,22 @@ const ResponsiveAppBar = () => {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex' } }} />
-          <Typography
-            variant="h5"
-            noWrap
-            sx={{
-              mr: 2,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            FILL MASTER
-          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex' } }}>
+            <Typography
+              variant="h5"
+              noWrap
+              sx={{
+                mr: 2,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              FILL MASTER
+            </Typography>
+          </Box>
 
           <Box sx={{ flexGrow: 0 }}>
             <IconButton
