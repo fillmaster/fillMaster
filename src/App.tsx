9c70385a1b8c7e5988c65954177b885445fff40c
Daniel Callus <danielcallus@gmail.com>
2022-05-31 08:37:56 +0100

    added header basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 import MenuOptions from './components/Menu';
 import StepMenu from './components/StepMenu';
 
 const App = () => {
   return (
     <>
+      <Header />
       <header className="App-header">Fill Master</header>
       <MenuOptions />
       <StepMenu />
