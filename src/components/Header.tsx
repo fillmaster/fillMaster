98bd0d1d9f242b457e1642ae083c702f1228b071
Daniel Callus <danielcallus@gmail.com>
2022-05-31 19:10:01 +0100

    centered text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,22 +11,21 @@ const ResponsiveAppBar = () => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex' } }}>
-            <Typography
-              variant="h5"
-              noWrap
-              sx={{
-                mr: 2,
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              FILL MASTER
-            </Typography>
-          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex' } }} />
+          <Typography
+            variant="h5"
+            noWrap
+            sx={{
+              mr: 2,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            FILL MASTER
+          </Typography>
 
           <Box sx={{ flexGrow: 0 }}>
             <IconButton
