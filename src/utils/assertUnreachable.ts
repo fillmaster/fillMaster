5a47f9e060cec3c6943f2a2346c2a974edeb0def
Daniel Callus <danielcallus@gmail.com>
2022-06-12 11:21:06 +0100

    previous

--- src/utils/assertUnreachable.ts
+++ src/utils/assertUnreachable.ts
@@ -1,5 +1,5 @@
 // This code can be used for the default value on a switch/case functions to ensure that all
-// cases of a union type or enum have been accounted for.
+// cases of a union type or enum have been accounted for. Pass the same param as used in the switch.
 
 function assertUnreachable(x: never): never {
   throw new Error("Didn't expect to get here");
