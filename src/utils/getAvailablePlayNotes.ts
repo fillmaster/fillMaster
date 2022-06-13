72ade3c6b973bfc2d3174e75d7afe642e1d813eb
Daniel Callus <danielcallus@gmail.com>
2022-06-13 20:43:31 +0100

    fixed available playnotes

--- src/utils/getAvailablePlayNotes.ts
+++ src/utils/getAvailablePlayNotes.ts
@@ -1,6 +1,6 @@
 import { BeatsPerBar } from '../consts/beatsPerBar';
 import { MeasureDivision, MEASURE_DIVISIONS } from '../consts/measureDivisions';
-import { stringToNumArray } from './stringNumConvert';
+import { numToStringArray, stringToNumArray } from './stringNumConvert';
 
 const getAvailablePlayNotes = (beatsPerBar_: BeatsPerBar, division_: MeasureDivision) => {
   const divisions = stringToNumArray([...MEASURE_DIVISIONS, '1']);
@@ -14,7 +14,7 @@ const getAvailablePlayNotes = (beatsPerBar_: BeatsPerBar, division_: MeasureDivi
     const remainder = Math.floor(beatsPerBar % rule);
     if (remainder === 0 && quotient >= 1) newArray.push(divisions[i]);
   }
-  return newArray;
+  return numToStringArray(newArray);
 };
 
 export default getAvailablePlayNotes;
