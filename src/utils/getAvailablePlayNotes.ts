8b9e9dbb592aa61c73b6441b30d216854c758675
Daniel Callus <danielcallus@gmail.com>
2022-06-13 21:14:18 +0100

    removed redundant multiplication from rule

--- src/utils/getAvailablePlayNotes.ts
+++ src/utils/getAvailablePlayNotes.ts
@@ -1,20 +1,21 @@
 import { BeatsPerBar } from '../consts/beatsPerBar';
 import { MeasureDivision, MEASURE_DIVISIONS } from '../consts/measureDivisions';
-import { numToStringArray, stringToNumArray } from './stringNumConvert';
+import { PlayNotes } from '../consts/playNotes';
+import { stringToNumArray } from './stringNumConvert';
 
 const getAvailablePlayNotes = (beatsPerBar_: BeatsPerBar, division_: MeasureDivision) => {
   const divisions = stringToNumArray([...MEASURE_DIVISIONS, '1']);
   const division = Number(division_);
   const beatsPerBar = Number(beatsPerBar_);
 
-  const newArray = [];
+  const newArray: Array<PlayNotes> = [];
   for (let i = 0; i < divisions.length - 1; i++) {
-    const rule = (beatsPerBar * division) / (beatsPerBar * divisions[i]);
+    const rule = division / divisions[i];
     const quotient = Math.floor(beatsPerBar / rule);
     const remainder = Math.floor(beatsPerBar % rule);
-    if (remainder === 0 && quotient >= 1) newArray.push(divisions[i]);
+    if (remainder === 0 && quotient >= 1) newArray.push(divisions[i].toString() as PlayNotes);
   }
-  return numToStringArray(newArray);
+  return newArray;
 };
 
 export default getAvailablePlayNotes;
