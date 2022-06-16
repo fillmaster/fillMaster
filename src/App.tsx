7d9e83cb65125a244557246ce7bb02b400434508
Daniel Callus <danielcallus@gmail.com>
2022-06-16 21:15:29 +0100

    dummy toggle in place

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import StepMenu from './components/StepMenu';
+import ToggleScreens from './components/ToggleScreens';
 
 const App = () => {
   return (
     <>
       <Header />
+      <ToggleScreens />
       <StepMenu />
     </>
   );
---
+++ src/components/ToggleScreens.tsx
@@ -0,0 +1,33 @@
+import '../App.css';
+
+import { useState } from 'react';
+
+const ToggleScreens = () => {
+  const [activeDiv, setActiveDiv] = useState('div1');
+
+  const toggleDiv = () => {
+    // eslint-disable-next-line no-unused-expressions
+    activeDiv === 'div1' ? setActiveDiv('div2') : setActiveDiv('div1');
+  };
+
+  return (
+    <>
+      <div className={activeDiv === 'div1' ? 'div1 show' : 'div1 hide'} id="div1">
+        Div 1
+        <button type="button" onClick={toggleDiv}>
+          {' '}
+          Hide div1
+        </button>
+      </div>
+      <div className={activeDiv === 'div2' ? 'div2 show' : 'div2 hide'} id="div2">
+        Div 2
+        <button type="button" onClick={toggleDiv}>
+          {' '}
+          Hide div2
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default ToggleScreens;
