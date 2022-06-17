30bc5344e8fce6cb60a2b5e1f709c2cc252e4af3
Daniel Callus <danielcallus@gmail.com>
2022-06-17 13:40:10 +0100

    updated to allow refresh randomiser

--- src/components/StepSetup.tsx
+++ src/components/StepSetup.tsx
@@ -1,10 +1,12 @@
-import { Typography } from '@mui/material';
+import RefreshIcon from '@mui/icons-material/RefreshRounded';
+import { Button, Typography } from '@mui/material';
+import { useState } from 'react';
 import beatIdeas from '../consts/forRandomiser/beatIdeas';
 import fillStarts from '../consts/forRandomiser/fillStarts';
 import fills from '../consts/forRandomiser/fillTypes';
+import getStringArrayBetweenTwoValues from '../utils/getArrayBetweenValues';
 import Randomiser from './Randomiser';
 import TempoChooser from './TempoChooser';
-import getStringArrayBetweenTwoValues from '../utils/getArrayBetweenValues';
 
 interface SetupStepProps {
   index: number;
@@ -25,6 +27,21 @@ const SetupStep = ({
   handleSetSliderValues,
   sliderValues,
 }: SetupStepProps) => {
+  const [beatIdeaKey, setBeatIdeaKey] = useState(0);
+  const [startFillKey, setStartFillKey] = useState(0);
+  const [fillKey, setFillKey] = useState(0);
+  const [tempoKey, setTempoKey] = useState(0);
+
+  type RandomiseMe = 'beatIdea' | 'startFill' | 'fill' | 'tempo';
+  const resetRandomiser = (randomsToReset: Array<RandomiseMe>) => {
+    if (randomsToReset.includes('beatIdea')) setBeatIdeaKey(beatIdeaKey + 1);
+    if (randomsToReset.includes('startFill')) setStartFillKey(startFillKey + 1);
+    if (randomsToReset.includes('fill')) setFillKey(fillKey + 1);
+    if (randomsToReset.includes('tempo')) setTempoKey(tempoKey + 1);
+  };
+
+  // const handleResetAllRandoms
+
   if (index === 0)
     return (
       <TempoChooser handleSetSliderValues={handleSetSliderValues} sliderValues={sliderValues} />
@@ -33,37 +50,64 @@ const SetupStep = ({
     return (
       <>
         <b>
-          <Randomiser arrayToRandomise={beatIdeas} handleSetItem={handleSetBeatIdea} />
+          <Randomiser
+            key={beatIdeaKey}
+            arrayToRandomise={beatIdeas}
+            handleSetItem={handleSetBeatIdea}
+          />
         </b>
         <div>When you&apos;ve thought of something, continue...</div>
+        <Button onClick={() => resetRandomiser(['beatIdea'])}>
+          <RefreshIcon />
+        </Button>
       </>
     );
   if (index === 2)
     return (
       <>
         <span>Start your drum fill on beat </span>
-        <Randomiser arrayToRandomise={fillStarts} handleSetItem={handleSetFillStart} />
-        <span> of bar 4.</span>
+        <Randomiser
+          key={startFillKey}
+          arrayToRandomise={fillStarts}
+          handleSetItem={handleSetFillStart}
+        />
+        <span> of bar 4.</span>{' '}
+        <span>
+          <Button onClick={() => resetRandomiser(['startFill'])}>
+            <RefreshIcon />
+          </Button>
+        </span>
       </>
     );
   if (index === 3)
     return (
-      <b>
-        <Randomiser arrayToRandomise={fills} handleSetItem={handleSetFill} />
-      </b>
+      <>
+        <b>
+          <Randomiser key={fillKey} arrayToRandomise={fills} handleSetItem={handleSetFill} />
+        </b>
+        <Button onClick={() => resetRandomiser(['fill'])}>
+          <RefreshIcon />
+        </Button>
+      </>
     );
   if (index === 4)
     return (
-      <b>
-        <Typography>
-          Tempo:{' '}
-          <Randomiser
-            arrayToRandomise={getStringArrayBetweenTwoValues(sliderValues[0], sliderValues[1])}
-            handleSetItem={handleSetTempo}
-          />{' '}
-          bpm
-        </Typography>
-      </b>
+      <>
+        <b>
+          <Typography>
+            Tempo:{' '}
+            <Randomiser
+              key={tempoKey}
+              arrayToRandomise={getStringArrayBetweenTwoValues(sliderValues[0], sliderValues[1])}
+              handleSetItem={handleSetTempo}
+            />{' '}
+            bpm
+          </Typography>
+        </b>
+        <Button onClick={() => resetRandomiser(['tempo'])}>
+          <RefreshIcon />
+        </Button>
+      </>
     );
   return null;
 };
