f205738c73382e40962ea23e8f500cffcc216980
Daniel Callus <danielcallus@gmail.com>
2022-06-17 21:22:18 +0100

    restartAnimation complete

--- src/components/QuickRandomiser.tsx
+++ src/components/QuickRandomiser.tsx
@@ -1,7 +1,8 @@
 //   const beatIdeaAsText = <Typography>{beatIdea}</Typography>;
 
 import RestartAllIcon from '@mui/icons-material/RestartAltRounded';
-import { Button, Typography } from '@mui/material';
+import { Button, Fade, Typography } from '@mui/material';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import BEAT_IDEAS from '../consts/forRandomiser/beatIdeas';
 import FILL_STARTS from '../consts/forRandomiser/fillStarts';
 import FILLS from '../consts/forRandomiser/fillTypes';
@@ -33,30 +34,79 @@ const QuickRandomiser = ({
   setTempo,
   tempoOptions,
 }: QuickRandomiserProps) => {
-  const quickResetRandomiser = (randomsToReset: Array<RandomiseMe>) => {
-    if (randomsToReset.includes('beatIdea')) setBeatIdea(shuffleArray([...BEAT_IDEAS])[0]);
-    if (randomsToReset.includes('startFill')) setFillStart(shuffleArray([...FILL_STARTS])[0]);
-    if (randomsToReset.includes('fill')) setFill(shuffleArray([...FILLS])[0]);
-    if (randomsToReset.includes('tempo')) setTempo(shuffleArray([...tempoOptions()])[0]);
+  const [beatIdeaVisible, setBeatIdeaVisible] = useState(false);
+  const [fillStartVisible, setFillStartVisible] = useState(false);
+  const [fillVisible, setFillVisible] = useState(false);
+  const [tempoVisible, setTempoVisible] = useState(false);
+
+  const handleChange = (setItem: Dispatch<SetStateAction<boolean>>) => {
+    setItem(false);
+    setTimeout(() => setItem(true), 600);
   };
+  const quickResetRandomiser = (randomsToReset: Array<RandomiseMe>) => {
+    if (randomsToReset.includes('beatIdea')) {
+      handleChange(setBeatIdeaVisible);
+      setBeatIdea(shuffleArray([...BEAT_IDEAS])[0]);
+    }
+    if (randomsToReset.includes('startFill')) {
+      handleChange(setFillStartVisible);
+      setFillStart(shuffleArray([...FILL_STARTS])[0]);
+    }
+    if (randomsToReset.includes('fill')) {
+      handleChange(setFillVisible);
+      setFill(shuffleArray([...FILLS])[0]);
+    }
+    if (randomsToReset.includes('tempo')) {
+      handleChange(setTempoVisible);
+      setTempo(shuffleArray([...tempoOptions()])[0]);
+    }
+  };
+
+  useEffect(() => {
+    setBeatIdeaVisible(false);
+    setTimeout(() => setBeatIdeaVisible(true), 600);
+  }, [beatIdea]);
+
+  useEffect(() => {
+    setFillStartVisible(false);
+    setTimeout(() => setFillStartVisible(true), 600);
+  }, [fillStart]);
+
+  useEffect(() => {
+    setFillVisible(false);
+    setTimeout(() => setFillVisible(true), 600);
+  }, [fill]);
+
+  useEffect(() => {
+    setTempoVisible(false);
+    setTimeout(() => setTempoVisible(true), 600);
+  }, [tempo]);
 
   return (
     <>
       <div>
-        <Typography>Drum Beat: {beatIdea}</Typography>
+        <Fade in={beatIdeaVisible}>
+          <Typography>Drum Beat: {beatIdea}</Typography>
+        </Fade>
       </div>
       <b>
         <div>
-          <Typography>Fill on Beat {fillStart} of bar 4</Typography>
+          <Fade in={fillStartVisible}>
+            <Typography>Fill on Beat {fillStart} of bar 4</Typography>
+          </Fade>
         </div>
       </b>
       <b>
         <div>
-          <Typography>Fill style: {fill}</Typography>
+          <Fade in={fillVisible}>
+            <Typography>Fill style: {fill}</Typography>
+          </Fade>
         </div>
       </b>
       <div>
-        <Typography>@ {tempo} bpm</Typography>
+        <Fade in={tempoVisible}>
+          <Typography>@ {tempo} bpm</Typography>
+        </Fade>
       </div>
       <Button onClick={() => quickResetRandomiser([...RANDOMISE_ME])}>
         <RestartAllIcon />
