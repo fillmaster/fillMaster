1fed3749f1de4290adb30f08f49d08dfa474b284
Daniel Callus <danielcallus@gmail.com>
2022-06-20 17:20:02 +0100

    added type for a function array

--- src/utils/getArrayBetweenValues.ts
+++ src/utils/getArrayBetweenValues.ts
@@ -1,5 +1,5 @@
 const getStringArrayBetweenTwoValues = (lowEnd: number, highEnd: number) => {
-  const list = [];
+  const list: string[] = [];
   for (let i = lowEnd; i <= highEnd; i++) {
     list.push(i.toString());
   }
