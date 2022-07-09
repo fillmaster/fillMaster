9ec57f8d31814c370e3b3977c378d101a0087745
Nico Kraetschmer <kraetschmerni@gmail.com>
2022-07-09 22:00:03 +0200

    added slowdown

--- src/components/Randomiser.tsx
+++ src/components/Randomiser.tsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from 'react';
 import shuffleArray from '../utils/randomFunctions';
 
 const DEFAULT_PAUSE_DURATION = 30;
-const DEFAULT_DURATION = 1000;
+const DEFAULT_DURATION = 1300;
+const SLOW_DOWN_THRESHOLD_FRACTION = 0.5;
+const ITERATION_SLOW_DOWN_MULTIPLIER = 70;
+// Smoothes out the animation and makes the animation less step
+const SLOW_DOWN_OVER_MULTIPLIER = 1.5;
 
 interface RandomAnimatorProps {
   arrayToRandomise: Array<string>;
@@ -20,33 +24,55 @@ async function flickThroughArray<T>(
   array: Array<T>,
   duration: number,
   pauseDuration: number,
-  callback: (value: T) => void
+  callback: (value: T) => void,
+  startDuration?: number
 ): Promise<void> {
   if (pauseDuration <= 0) {
     throw new Error('pauseDuration must be greater than 0');
   }
 
+  let slowedPauseDuration = pauseDuration;
   let timeSpend = 0;
 
+  const actualStartDuration = startDuration ?? duration;
+
   for (let i = 0; i < array.length; i++) {
+    const timeLeft = duration - timeSpend;
+    // We don't want to overshoot
+    const actualPauseDuration = Math.min(timeLeft, slowedPauseDuration);
+    // the true time we already spent in the loop with recursion
+    const timeSpentTotal = actualStartDuration - duration + timeSpend;
+
     if (timeSpend >= duration) {
       return Promise.resolve();
     }
-    // We don't want to overshoot
-    const actualPauseDuration =
-      duration - timeSpend > pauseDuration ? pauseDuration : duration - timeSpend;
-
+    // eslint-disable-next-line no-await-in-loop
+    await sleep(actualPauseDuration);
     // Maybe the callback becomes undefined
     // if the component unmounts before the loop is finished
     // In that case we just cancel the flicking
     if (typeof callback === 'undefined') return Promise.resolve();
-
     callback(array[i]);
-    // eslint-disable-next-line no-await-in-loop
-    await sleep(actualPauseDuration);
+
+    // Slowdown
+    if (timeSpentTotal >= SLOW_DOWN_THRESHOLD_FRACTION * actualStartDuration) {
+      // this is basically 1/totalTimeLeft so we get an exponential slow down
+      const exponential = SLOW_DOWN_OVER_MULTIPLIER / (actualStartDuration - timeSpentTotal);
+      // We use a multiplier to make these values bigger
+      const addAmount = slowedPauseDuration * exponential * ITERATION_SLOW_DOWN_MULTIPLIER;
+
+      slowedPauseDuration += addAmount;
+    }
+
     timeSpend += actualPauseDuration;
   }
-  return flickThroughArray(array, duration - timeSpend, pauseDuration, callback);
+  return flickThroughArray(
+    array,
+    duration - timeSpend,
+    slowedPauseDuration,
+    callback,
+    startDuration ?? duration
+  );
 }
 
 const Randomiser = ({
