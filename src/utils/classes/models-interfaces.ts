8f151391b4401015338e3caa2296bcd6510ba78d
ninad <programmer211990@gmail.com>
2022-07-14 20:55:34 +0530

    review comments

--- src/utils/classes/models-interfaces.ts
+++ src/utils/classes/models-interfaces.ts
@@ -1,5 +1,6 @@
+import { BeatsPerBar } from "../../consts/beatsPerBar";
 import { PlayNotes } from "../../consts/playNotes";
-import { PlayFillOn, TimeSignature } from "./patternMaker";
+import { TimeSignature } from "./patternMaker";
 
 export type SubBeatPosition = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7';
 
@@ -11,10 +12,13 @@ export enum MetronomeSounds {
 }
 
 export interface IPatternSettings {
-  playNotes: PlayNotes;
-  playFillOn: PlayFillOn;
-  timeSignature: TimeSignature;
+	playNotes: PlayNotes;
+	timeSignature: TimeSignature;
 }
+
+export type BeatPosition = BeatsPerBar;
+
+export type PlayFillOn = { beat: BeatPosition; subBeat: SubBeatPosition };
 
 // Kept as union of numbers instead of strings as this will never be set by the UI directly.
 // This is only used for calculations and will not be directly part of a metronomeString.
--- src/utils/classes/pattern.ts
+++ src/utils/classes/pattern.ts
@@ -1,19 +1,16 @@
 import { IPatternSettings } from "./models-interfaces";
 
 export class Pattern {
-	// metronome sound should not be here?
-	// private metronomeSound: MetronomeSounds = MetronomeSounds.MetronomeSoundOff;
 
 	private defaultPatternSettings: IPatternSettings = {
 		playNotes: 'quarterNotes',
-		playFillOn: { beat: '3', subBeat: '0' }, // can we make these objects into classes of their own or will it be overkill?
-		timeSignature: { beats: '4', division: '4' }, // can we make these objects into classes of their own or will it be overkill?
-	};;
+		timeSignature: { beats: '4', division: '4' },
+	};
 
 	private patternSettings: IPatternSettings; // user provided settings
 
 	constructor(patternSettings?: IPatternSettings) {
-		this.patternSettings = patternSettings ? { ...this.getDefaultPatternSettings(), ...patternSettings} : this.getDefaultPatternSettings(); 
+		this.patternSettings = patternSettings ? { ...this.getDefaultPatternSettings(), ...patternSettings } : this.getDefaultPatternSettings();
 	}
 
 	getDefaultPatternSettings(): IPatternSettings {
@@ -23,7 +20,7 @@ export class Pattern {
 	getPatternSettings(): IPatternSettings {
 		return this.patternSettings;
 	}
-	
+
 	setPatternSettings(patternSettings: IPatternSettings): void {
 		this.patternSettings = patternSettings;
 	}
