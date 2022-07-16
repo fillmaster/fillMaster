c8d09f2be67097ac34e937c729d13ca714d4b06c
Daniel Callus <danielcallus@gmail.com>
2022-07-16 13:30:24 +0100

    reset accidental change

--- src/classes/pattern/pattern.ts
+++ src/classes/pattern/pattern.ts
@@ -6,11 +6,11 @@ import {
 } from './models-interfaces';
 
 class Pattern {
+  protected subDivision: SubDivision;
 
   protected defaultPatternSettings: IPatternSettings = {
     playNotes: 'quarterNotes',
     timeSignature: { beats: '4', division: '4' },
-    subDivision = 8;
   };
 
   private patternSettings: IPatternSettings; // user provided settings
@@ -19,10 +19,11 @@ class Pattern {
     this.patternSettings = patternSettings
       ? { ...this.getDefaultPatternSettings(), ...patternSettings }
       : this.getDefaultPatternSettings();
+    this.subDivision = subDivision;
   }
 
   public getSubDivision = () => {
-    return this.patternSettings.subDivision;
+    return this.subDivision;
   };
 
   public resetTimeSignature = () => {
